46b32821ad3d1ec02076a91669b50af760971a47
임채성 <pan1129@naver.com>
2022-04-09 17:42:52 +0900

    변수 데이터 값 Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int count = 4;
+        int price = 3;
+        int sum = 0;
+        int answer = 0;
+        int money = 20;
+        int[] arr = new int[count];
+
+        for(int i=0; i<=arr.length; i++){
+            arr[i] += price;
+            sum += arr[i];
+            answer = sum - money;
+            System.out.println(answer);
+        }
+
     }
 }
---
+++ src/com/company/variable/Ex2_4.java
@@ -0,0 +1,11 @@
+package com.company.variable;
+
+public class Ex2_4 {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x);
+
+        x = 10;
+        System.out.println(x);
+    }
+}
