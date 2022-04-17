3bca196d053d097879c9c6db7ca7497aed41a739
임채성 <pan1129@naver.com>
2022-04-17 14:26:07 +0900

    부호 연산자 Commit

---
+++ src/com/company/operator/Ex3_3.java
@@ -0,0 +1,11 @@
+package com.company.operator;
+
+public class Ex3_3 {
+    public static void main(String[] args) {
+        int i=5, j=5;
+        System.out.println(i++); // i의 값을 출력 후 1증가
+        System.out.println(++j); // j의 값을 1증가 후,출력
+        System.out.println("i = " + i + ", j=" + j);
+
+    }
+}
