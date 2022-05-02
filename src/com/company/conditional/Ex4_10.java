77b469ac4fcecdf6ab0209451db235c549fb584c
임채성 <pan1129@naver.com>
2022-05-02 22:03:33 +0900

    for문 Commit

---
+++ src/com/company/conditional/Ex4_10.java
@@ -0,0 +1,12 @@
+package com.company.conditional;
+
+public class Ex4_10 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for(int i=1; i<=5; i++){
+            sum += i;
+            System.out.printf("1부터 %2d 까지의 합: %2d%n", i, sum);
+        }
+    }
+}
